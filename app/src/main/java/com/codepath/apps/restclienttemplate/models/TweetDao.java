78380db0569c21954e7f117943803c9e7a9f275f
xmantra23 <samir.shrestha92@gmail.com>
2021-10-03 20:37:07 -0500

    walthrough added.

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDao.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TweetDao.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Dao
 public interface TweetDao {
     @Query("SELECT Tweet.body AS tweet_body, Tweet.createdAt AS tweet_createdAt , Tweet.id AS tweet_id,User.*" +
-            " FROM Tweet INNER JOIN User ON Tweet.userId = User.id ORDER BY Tweet.createdAt DESC LIMIT 5")
+            " FROM Tweet INNER JOIN User ON Tweet.userId = User.id ORDER BY Tweet.createdAt DESC LIMIT 20")
     List<TweetWithUser> recentItems();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
